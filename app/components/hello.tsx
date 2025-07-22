47ff59118e45ce13508c3699239d20a57ac1164e
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2025-07-22 20:41:37 +0300

    * nextjs practices

---
+++ app/components/hello.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+function Hello() {
+    console.log('I am a client component!');
+    
+  return (
+    <div className="text-3xl">
+      Hello Next.js 🫶
+    </div>
+  );
+    
+}
+
+export default Hello;
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Hello from "./components/hello";
+
+
+export default function Home() {
+  console.log('What am I doing here? -- SERVER/CLIENT');
+
+  return (
+    <>
+      <h1 className="text-3xl">Welcome to Next.js</h1>
+      <Hello />
+    </>
+  );
+}
