aa49aac7ecb2ae7990cab66505b28d4f6a0d4678
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2025-07-23 19:23:02 +0300

    * practices

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
-import Hello from "./components/hello";
+import Hello from "../components/hello";
 
 
 export default function Home() {
   console.log('What am I doing here? -- SERVER/CLIENT');
-
+  /*   throw new Error('NOT IMPLEMENTED YET');
+   */
   return (
     <>
       <h1 className="text-3xl">Welcome to Next.js</h1>
